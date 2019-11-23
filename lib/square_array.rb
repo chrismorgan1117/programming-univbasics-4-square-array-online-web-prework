78802acc0b6ccb1e196b6596ea37c46c588609ac
Christopher Morgan <cwm111794@gmail.com>
2019-11-23 00:53:29 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,15 @@
+new_numbers = [9,10,16,25]
+numbers = [1,2,3]
+
+
 def square_array(array)
-  # your code here
+  count = 0 
+  while array.fetch(count) != nil do 
+    num2 = array.fetch(count) ** 2 
+    array.insert(count, num2)
+    count += 1 
+  end
 end
+ 
+ square_array(numbers)
+ square_array(new_numbers)
